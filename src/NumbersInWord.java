a968e2595b53f39c33d4335eb402ed9bc845e198
josephyao <dermott@126.com>
2014-02-01 12:18:18 +0800

    Test: add the rest of single digit cases TPP: constant -> constants (in array WORDS)

--- src/NumbersInWord.java
+++ src/NumbersInWord.java
@@ -1,6 +1,9 @@
 public class NumbersInWord {
 
-    public static final String[] WORDS = new String[]{"zero", "one", "two"};
+    public static final String[] WORDS = new String[]{
+            "zero", "one", "two", "three", "four", "five",
+            "six", "seven", "eight", "nine"
+    };
 
     public String convert(int number) {
         return WORDS[number];
--- src/TestNumbersInWord.java
+++ src/TestNumbersInWord.java
@@ -11,5 +11,12 @@ public class TestNumbersInWord {
         assertEquals("zero", convertor.convert(0));
         assertEquals("one", convertor.convert(1));
         assertEquals("two", convertor.convert(2));
+        assertEquals("three", convertor.convert(3));
+        assertEquals("four", convertor.convert(4));
+        assertEquals("five", convertor.convert(5));
+        assertEquals("six", convertor.convert(6));
+        assertEquals("seven", convertor.convert(7));
+        assertEquals("eight", convertor.convert(8));
+        assertEquals("nine", convertor.convert(9));
     }
 }

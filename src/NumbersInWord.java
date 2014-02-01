bcdb693e9ebeff0af562a813493c832af4a38ea1
josephyao <dermott@126.com>
2014-02-01 15:08:25 +0800

    Test: add two digits convert to one word cases TPP: constant -> constants (in array WORDS)

--- src/NumbersInWord.java
+++ src/NumbersInWord.java
@@ -2,7 +2,9 @@ public class NumbersInWord {
 
     public static final String[] WORDS = new String[]{
             "zero", "one", "two", "three", "four", "five",
-            "six", "seven", "eight", "nine"
+            "six", "seven", "eight", "nine", "ten",
+            "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+            "sixteen", "seventeen", "eighteen", "nineteen", "twenty"
     };
 
     public String convert(int number) {

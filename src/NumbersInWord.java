d86299e1b23cd0c57533183b079abf32ee27beed
josephyao <dermott@126.com>
2014-02-07 11:50:30 +0800

    Test: 30. Let's back to this case and think about another transformation. TPP: array -> container. Few things to note here: 1. I refactor the if condition (number > 20) to (number <= 20) so that return WORDS[number] becomes the first execution path 2. I refactor the array to a Map which contains the same data as array. Only thing changed here is the array index now becomes the map key. 3. Then, I transform the code to add another pair (30 -> "thirty") into map to pass the test case 30. At this step, I need to change the if condition to WORDS.containKey(number). I treat this change and the new key-value pair both as a necessary part of this array -> container transformation. With them, the code is "really" using a container now.

--- src/NumbersInWord.java
+++ src/NumbersInWord.java
@@ -1,16 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class NumbersInWord {
 
-    public static final String[] WORDS = new String[]{
-            "zero", "one", "two", "three", "four", "five",
-            "six", "seven", "eight", "nine", "ten",
-            "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-            "sixteen", "seventeen", "eighteen", "nineteen", "twenty"
-    };
+    public static final Map<Integer, String> WORDS = new HashMap<Integer, String>() {{
+        put(0, "zero"); put(1, "one"); put(2, "two"); put(3, "three");
+        put(4, "four"); put(5, "five"); put(6, "six"); put(7, "seven");
+        put(8, "eight"); put(9, "nine"); put(10, "ten"); put(11, "eleven");
+        put(12, "twelve"); put(13, "thirteen"); put(14, "fourteen"); put(15, "fifteen");
+        put(16, "sixteen"); put(17, "seventeen"); put(18, "eighteen"); put(19, "nineteen");
+        put(20, "twenty"); put(30, "thirty");
+    }};
 
     public String convert(int number) {
-        if (number > 20)
-            return WORDS[20] + " " + WORDS[number - 20];
+        if (WORDS.containsKey(number))
+            return WORDS.get(number);
 
-        return WORDS[number];
+        return WORDS.get(20) + " " + WORDS.get(number - 20);
     }
 }

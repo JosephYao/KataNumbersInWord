fb83a105d489d796a2dbb7862e29b9fc20ee248a
josephyao <dermott@126.com>
2014-02-15 12:57:05 +0800

    Test: 30, passed it again with a new transformation TPP: array -> container (WORDS array -> NUMBER_TO_ONE_WORD map). And, the if condition was changed to "NUMBER_TO_ONE_WORD.containsKey" which quite reasonable when applying this transformation What does this transformation mean? array -> container gets a simpler and better code than the original unconditional -> if transformation. So, does this mean array -> container should have a higher priority? I think so.

--- src/NumbersInWord.java
+++ src/NumbersInWord.java
@@ -3,40 +3,17 @@ import java.util.Map;
 
 public class NumbersInWord {
 
-    private static final String[] WORDS = new String[]{
-            "zero", "one", "two", "three", "four",
-            "five", "six", "seven", "eight", "nine",
-            "ten", "eleven", "twelve", "thirteen", "fourteen",
-            "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",
-            "twenty"
-    };
-
     private static final Map<Integer, String> NUMBER_TO_ONE_WORD = new HashMap<Integer, String>(){{
-        put(0, "zero");
-        put(1, "one");
-        put(2, "two");
-        put(3, "three");
-        put(4, "four");
-        put(5, "five");
-        put(6, "six");
-        put(7, "seven");
-        put(8, "eight");
-        put(9, "nine");
-        put(10, "ten");
-        put(11, "eleven");
-        put(12, "twelve");
-        put(13, "thirteen");
-        put(14, "fourteen");
-        put(15, "fifteen");
-        put(16, "sixteen");
-        put(17, "seventeen");
-        put(18, "eighteen");
-        put(19, "nineteen");
-        put(20, "twenty");
+        put(0, "zero"); put(1, "one"); put(2, "two"); put(3, "three"); put(4, "four");
+        put(5, "five"); put(6, "six"); put(7, "seven"); put(8, "eight"); put(9, "nine");
+        put(10, "ten"); put(11, "eleven"); put(12, "twelve"); put(13, "thirteen");
+        put(14, "fourteen"); put(15, "fifteen"); put(16, "sixteen"); put(17, "seventeen");
+        put(18, "eighteen"); put(19, "nineteen");
+        put(20, "twenty"); put(30, "thirty");
     }};
 
     public String convert(int number) {
-        if (number <= 20)
+        if (NUMBER_TO_ONE_WORD.containsKey(number))
             return NUMBER_TO_ONE_WORD.get(number);
 
         return NUMBER_TO_ONE_WORD.get(20) + " " + NUMBER_TO_ONE_WORD.get(number - 20);

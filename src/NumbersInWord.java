78f63ac34b2726aba1578acedcb18f41f819536b
josephyao <dermott@126.com>
2014-02-15 12:12:36 +0800

    Let's ignore current order of TPP for now and think about a simpler way to pass case 30. A simpler way I see here is to use Map instead of array so that 30 can be a part of Map and then the case can be passed. So, that's the refactoring happened that. Another refactoring I did is that I changed the order of two execution path by reverse the if condition to "number <= 20".

--- src/NumbersInWord.java
+++ src/NumbersInWord.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class NumbersInWord {
 
     private static final String[] WORDS = new String[]{
@@ -8,13 +11,34 @@ public class NumbersInWord {
             "twenty"
     };
 
+    private static final Map<Integer, String> NUMBER_TO_ONE_WORD = new HashMap<Integer, String>(){{
+        put(0, "zero");
+        put(1, "one");
+        put(2, "two");
+        put(3, "three");
+        put(4, "four");
+        put(5, "five");
+        put(6, "six");
+        put(7, "seven");
+        put(8, "eight");
+        put(9, "nine");
+        put(10, "ten");
+        put(11, "eleven");
+        put(12, "twelve");
+        put(13, "thirteen");
+        put(14, "fourteen");
+        put(15, "fifteen");
+        put(16, "sixteen");
+        put(17, "seventeen");
+        put(18, "eighteen");
+        put(19, "nineteen");
+        put(20, "twenty");
+    }};
+
     public String convert(int number) {
-        if (number == 30)
-            return "thirty";
+        if (number <= 20)
+            return NUMBER_TO_ONE_WORD.get(number);
 
-        if (number > 20)
-            return WORDS[20] + " " + WORDS[number - 20];
-
-        return WORDS[number];
+        return NUMBER_TO_ONE_WORD.get(20) + " " + NUMBER_TO_ONE_WORD.get(number - 20);
     }
 }

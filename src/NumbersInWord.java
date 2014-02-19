37974767a0e1d1e5aa9accee609aef82949e7561
josephyao <dermott@126.com>
2014-02-19 22:49:48 +0800

    Refactoring: the duplication of "thousand" and "hundred" code has been removed by a new counting unit map and a for loop

--- src/NumbersInWord.java
+++ src/NumbersInWord.java
@@ -1,10 +1,10 @@
-import java.util.NavigableMap;
-import java.util.TreeMap;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NumbersInWord {
 
     private static final String DELIMITER = " ";
-    private static final NavigableMap<Integer, String> NUMBER_TO_ONE_WORD = new TreeMap<Integer, String>(){{
+    private static final Map<Integer, String> NUMBER_TO_ONE_WORD = new HashMap<Integer, String>(){{
         put(0, "zero"); put(1, "one"); put(2, "two"); put(3, "three"); put(4, "four");
         put(5, "five"); put(6, "six"); put(7, "seven"); put(8, "eight"); put(9, "nine");
         put(10, "ten"); put(11, "eleven"); put(12, "twelve"); put(13, "thirteen");
@@ -15,15 +15,19 @@ public class NumbersInWord {
         put(90, "ninety");
     }};
 
+    private static final Map<Integer, String> COUNTING_UNIT_TO_ONE_WORD = new HashMap<Integer, String>(){{
+        put(100, "hundred");
+        put(1000, "thousand");
+    }};
+
     public String convert(int number) {
         if (NUMBER_TO_ONE_WORD.containsKey(number))
             return NUMBER_TO_ONE_WORD.get(number);
 
-        if (number % 1000 == 0)
-            return NUMBER_TO_ONE_WORD.get(number / 1000) + DELIMITER + "thousand";
-
-        if (number % 100 == 0)
-            return NUMBER_TO_ONE_WORD.get(number / 100) + DELIMITER + "hundred";
+        for (Integer countingUnit : COUNTING_UNIT_TO_ONE_WORD.keySet())
+            if (number % countingUnit == 0)
+                return NUMBER_TO_ONE_WORD.get(number / countingUnit) + DELIMITER +
+                       COUNTING_UNIT_TO_ONE_WORD.get(countingUnit);
 
         return NUMBER_TO_ONE_WORD.get(20) + DELIMITER +
                NUMBER_TO_ONE_WORD.get(number - 20);

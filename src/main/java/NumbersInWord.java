3c81d52d97ecba83609fb56c551ce7ca35569d55
josephyao <dermott@126.com>
2014-10-03 13:25:30 +0800

    1 convert to one TPP: unconditional -> if

--- src/main/java/NumbersInWord.java
+++ src/main/java/NumbersInWord.java
@@ -1,6 +1,9 @@
 public class NumbersInWord {
 
     public String convert(int number) {
+        if (number == 1)
+            return "one";
+
         return "zero";
     }
 }
--- src/test/java/TestNumbersInWord.java
+++ src/test/java/TestNumbersInWord.java
@@ -4,9 +4,15 @@ import static org.junit.Assert.assertEquals;
 
 public class TestNumbersInWord {
 
+    NumbersInWord convertor = new NumbersInWord();
+
     @Test
     public void _0_convert_to_zero() {
-        NumbersInWord convertor = new NumbersInWord();
         assertEquals("zero", convertor.convert(0));
+    }
+
+    @Test
+    public void _1_convert_to_one() {
+        assertEquals("one", convertor.convert(1));
     }
 }

a571d6123dd36a3205961e31cb4f9793b98f3f5b
josephyao <dermott@126.com>
2014-10-05 13:10:40 +0800

    Revert "number convert to one word"
    This reverts commit 80f86627dd28361691a6de14f52a2d9fc20605bc.

--- src/main/java/NumbersInWord.java
+++ src/main/java/NumbersInWord.java
@@ -1,10 +1,7 @@
 public class NumbersInWord {
 
     public String convert(int number) {
-        String[] numberWords = {"zero", "one", "two", "three", "four", "five",
-        "six", "seven", "eight", "nine", "ten",
-        "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-        "sixteen", "seventeen", "eighteen", "nineteen", "twenty"};
+        String[] numberWords = {"zero", "one", "two"};
 
         if (number > 20)
             return numberWords[20] + " " + numberWords[number - 20];

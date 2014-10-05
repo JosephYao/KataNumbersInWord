a5623472118534c4544d556274887ab1f73403d4
josephyao <dermott@126.com>
2014-10-05 13:12:35 +0800

    Revert "Revert "number convert to one word""
    This reverts commit a571d6123dd36a3205961e31cb4f9793b98f3f5b.

--- src/main/java/NumbersInWord.java
+++ src/main/java/NumbersInWord.java
@@ -1,7 +1,10 @@
 public class NumbersInWord {
 
     public String convert(int number) {
-        String[] numberWords = {"zero", "one", "two"};
+        String[] numberWords = {"zero", "one", "two", "three", "four", "five",
+        "six", "seven", "eight", "nine", "ten",
+        "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+        "sixteen", "seventeen", "eighteen", "nineteen", "twenty"};
 
         if (number > 20)
             return numberWords[20] + " " + numberWords[number - 20];
--- src/test/java/TestNumbersInWord.java
+++ src/test/java/TestNumbersInWord.java
@@ -7,10 +7,28 @@ public class TestNumbersInWord {
     NumbersInWord convertor = new NumbersInWord();
 
     @Test
-    public void single_digit_convert_to_one_word() {
+    public void number_convert_to_one_word() {
         assertEquals("zero", convertor.convert(0));
         assertEquals("one", convertor.convert(1));
         assertEquals("two", convertor.convert(2));
+        assertEquals("three", convertor.convert(3));
+        assertEquals("four", convertor.convert(4));
+        assertEquals("five", convertor.convert(5));
+        assertEquals("six", convertor.convert(6));
+        assertEquals("seven", convertor.convert(7));
+        assertEquals("eight", convertor.convert(8));
+        assertEquals("nine", convertor.convert(9));
+        assertEquals("ten", convertor.convert(10));
+        assertEquals("eleven", convertor.convert(11));
+        assertEquals("twelve", convertor.convert(12));
+        assertEquals("thirteen", convertor.convert(13));
+        assertEquals("fourteen", convertor.convert(14));
+        assertEquals("fifteen", convertor.convert(15));
+        assertEquals("sixteen", convertor.convert(16));
+        assertEquals("seventeen", convertor.convert(17));
+        assertEquals("eighteen", convertor.convert(18));
+        assertEquals("nineteen", convertor.convert(19));
+        assertEquals("twenty", convertor.convert(20));
     }
 
     @Test
